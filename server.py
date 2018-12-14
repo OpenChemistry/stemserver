07628923b480297a67c925521c34a903deaa3d25
Chris Harris <chris.harris@kitware.com>
2018-12-14 16:32:01 -0500

    Stream partial images

--- server.py
+++ server.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+import glob
+import os
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 @socketio.on('connect', namespace='/stem')
 def connect():
     print('Client connected')
-    with open('raw.bin', 'rb') as f:
-        emit('stem', {'data': f.read()})
+
+    for path in glob.glob('./data/stem0.*.bin'):
+        with open(path, 'rb') as f:
+            emit('stem', {
+                'data': f.read()
+            });
 
 @socketio.on('disconnect', namespace='/stem')
 def disconnect():
