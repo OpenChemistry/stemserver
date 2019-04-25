8970951a0a12858b1aca184184614518d66f660d
Chris Harris <chris.harris@kitware.com>
2019-04-25 13:53:56 -0400

    Fix URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'stemserver',
     version = '0.0.1',
-    url = 'https://github.com/cjh1/stemserver.git',
+    url = 'https://github.com/OpenChemistry/stemserver.git',
     description = 'Flask SockIO app to server STEM data.',
     packages = find_packages(),
     install_requires = [
