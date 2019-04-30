df76941c44e2ca98b78525931b850b287ec5298b
Patrick Avery <patrick.avery@kitware.com>
2019-04-30 13:50:11 -0400

    Store images created with `filePath` on girder
    Now, if a user creates a stem image with a `filePath` parameter:
    
    1. The user must be an administrator (no one else should be able to read
    files on the server).
    2. The file is imported to the girder server and put in a private
    folder called `stem_images` in the root folder of the user who made it.
    If the folder already exists, it will be re-used.
    3. The file is put in an item called `_imported_images`. The names of
    the files are all just indices (starting with 1). If 1000 files exist,
    an error will occur when trying to import another one.
    4. When the stem image is deleted, if it was an imported file, the
    imported file is deleted as well.
    
    These changes allow us to enforce all stem images to have a girder
    `fileId`.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- girder/stemserver_plugin/models/stemimage.py
+++ girder/stemserver_plugin/models/stemimage.py
@@ -1,24 +1,30 @@
-import os
-
 import h5py
 import numpy as np
 
 from girder.api.rest import setResponseHeader, RestException
-from girder.constants import AccessType
+from girder.constants import AccessType, AssetstoreType
+from girder.models.assetstore import Assetstore as AssetstoreModel
 from girder.models.file import File as FileModel
+from girder.models.folder import Folder as FolderModel
+from girder.models.item import Item as ItemModel
 from girder.models.model_base import AccessControlledModel
+from girder.utility.filesystem_assetstore_adapter import (
+    FilesystemAssetstoreAdapter
+)
 
 
 class StemImage(AccessControlledModel):
+
+    IMPORT_FOLDER = 'stem_images'
+    IMPORT_ITEM_NAME = '_imported_images'
 
     def __init__(self):
         super(StemImage, self).__init__()
 
     def initialize(self):
         self.name = 'stemimages'
-
-        self.exposeFields(level=AccessType.READ, fields=(
-           '_id', 'filePath', 'fileId'))
+        self.ensureIndices(('_id', 'fileId'))
+        self.exposeFields(level=AccessType.READ, fields=('_id', 'fileId'))
 
     def filter(self, stem_image, user):
         stem_image = super(StemImage, self).filter(doc=stem_image, user=user)
@@ -29,17 +35,11 @@ class StemImage(AccessControlledModel):
         return stem_image
 
     def validate(self, doc):
-        # Ensure the fileId or filePath are valid
+        # Ensure the file exists
         if 'fileId' in doc:
-            file = FileModel().load(doc['fileId'], level=AccessType.READ)
-            doc['fileId'] = file['_id']
-        elif 'filePath' in doc:
-            if not os.path.isfile(doc['filePath']):
-                msg = 'File does not exist: ' + doc['filePath']
-                raise RestException(msg, code=400)
+            FileModel().load(doc['fileId'], level=AccessType.READ, force=True)
         else:
-            raise RestException('Neither fileId nor filePath are set',
-                                code=400)
+            raise RestException('stem image must contain `fileId`', code=400)
 
         return doc
 
@@ -48,7 +48,19 @@ class StemImage(AccessControlledModel):
         if file_id:
             stem_image['fileId'] = file_id
         elif file_path:
-            stem_image['filePath'] = file_path
+            # Only admin users can do this
+            admin = user.get('admin', False)
+            if admin is not True:
+                raise RestException('Only admin users can use a filePath',
+                                    code=403)
+
+            item = self._get_import_stem_item(user)
+            name = self._get_import_unique_file_name(item)
+            assetstore = self._get_assetstore()
+
+            adapter = FilesystemAssetstoreAdapter(assetstore)
+            f = adapter.importFile(item, file_path, user, name)
+            stem_image['fileId'] = f['_id']
         else:
             raise RestException('Must set either fileId or filePath',
                                 code=400)
@@ -59,22 +71,31 @@ class StemImage(AccessControlledModel):
 
         return self.save(stem_image)
 
-    def _get_file(self, stem_image):
-        """Get the file object or file path of the stem image.
+    def delete(self, id, user):
+        stem_image = self.load(id, user=user, level=AccessType.WRITE)
 
-        If there is a fileId in stem_image, returns a file object.
-        If there is a filePath in stem_image, returns the path.
+        if not stem_image:
+            raise RestException('StemImage not found.', code=404)
 
-        """
-        if 'fileId' in stem_image:
-            girder_file = FileModel().load(stem_image['fileId'],
-                                           level=AccessType.READ)
-            return FileModel().open(girder_file)
-        elif 'filePath' in stem_image:
-            return stem_image['filePath']
+        # Try to load the file and check if it was imported.
+        # If it was imported, delete the imported file.
+        # If loading the file fails, remove the stem_image anyways
+        try:
+            f = FileModel().load(stem_image['fileId'], level=AccessType.READ,
+                                 user=user)
+            if f.get('imported', False) is True:
+                if f['itemId'] == self._get_import_stem_item(user)['_id']:
+                    FileModel().remove(f)
+        except:
+            pass
 
-        raise RestException('StemImage does not contain `fileId` nor '
-                            '`filePath`.', code=400)
+        return self.remove(stem_image)
+
+    def _get_file(self, stem_image, user):
+        """Get a file object of the stem image"""
+        girder_file = FileModel().load(stem_image['fileId'],
+                                       level=AccessType.READ, user=user)
+        return FileModel().open(girder_file)
 
     def _get_h5_dataset(self, id, user, path):
         stem_image = self.load(id, user=user, level=AccessType.READ)
@@ -82,7 +103,7 @@ class StemImage(AccessControlledModel):
         if not stem_image:
             raise RestException('StemImage not found.', code=404)
 
-        f = self._get_file(stem_image)
+        f = self._get_file(stem_image, user)
 
         setResponseHeader('Content-Type', 'application/octet-stream')
 
@@ -120,8 +141,7 @@ class StemImage(AccessControlledModel):
         if not stem_image:
             raise RestException('StemImage not found.', code=404)
 
-        f = self._get_file(stem_image)
-
+        f = self._get_file(stem_image, user)
         with h5py.File(f, 'r') as f:
             return f[path].shape
 
@@ -130,3 +150,35 @@ class StemImage(AccessControlledModel):
 
     def dark_shape(self, id, user):
         return self._get_h5_dataset_shape(id, user, '/stem/dark')
+
+    def _get_import_stem_item(self, user):
+        """Get the item where we will store stem images
+
+        A new folder and item will be created if they do not exist.
+        Otherwise, they will be reused.
+
+        """
+        folder = FolderModel().createFolder(user, StemImage.IMPORT_FOLDER,
+                                            parentType='user', public=False,
+                                            creator=user, reuseExisting=True)
+
+        return ItemModel().createItem(StemImage.IMPORT_ITEM_NAME, user, folder,
+                                      reuseExisting=True)
+
+    def _get_import_unique_file_name(self, item):
+        """Get a unique name of a file to go in the item"""
+        files = ItemModel().childFiles(item)
+        names = [x['name'] for x in files]
+        for i in range(1, 1000):
+            if str(i) not in names:
+                return str(i)
+
+        raise RestException('Too many files in item:\n' + str(item), code=400)
+
+    def _get_assetstore(self):
+        """Gets the asset store and ensures it is a file system"""
+        assetstore = AssetstoreModel().getCurrent()
+        if assetstore['type'] is not AssetstoreType.FILESYSTEM:
+            raise RestException('Current assetstore is not a file system!',
+                                code=400)
+        return assetstore
