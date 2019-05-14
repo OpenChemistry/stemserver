1edefb6b2c992221fec72e96f35837f1761cb679
Chris Harris <chris.harris@kitware.com>
2019-05-14 17:00:13 -0400

    Use flask-login to login to Girder

--- flask/server.py
+++ flask/server.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room
+from flask_socketio import SocketIO
+from flask import Flask
+from flask_login import LoginManager, login_required, login_user
 
 import glob
 import os
 
+from stemserver.girder.auth import _fetch_girder_user_from_token, auth_blueprint
 from stemserver.socketio import endpoints as socketio_endpoints
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+login_manager = LoginManager()
+login_manager.init_app(app)
 socketio = SocketIO(app)
 
+app.register_blueprint(auth_blueprint)
 
-@socketio.on('subscribe', namespace='/stem')
-def subscribe(topic):
-    join_room(topic)
+# Girder authentication
+@login_manager.user_loader
+def load_user(girder_token):
+    return _fetch_girder_user_from_token(girder_token)
 
 # Setup the socketio events
 socketio_endpoints.init(socketio)
