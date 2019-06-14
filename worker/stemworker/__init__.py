78f67da5c763042e04ee215b63ca3f962abc0809
Chris Harris <chris.harris@kitware.com>
2019-06-14 16:57:07 -0400

    Add basic loading on an extension

--- worker/stemworker/__init__.py
+++ worker/stemworker/__init__.py
@@ -1,4 +1,13 @@
 import aiohttp
+import logging
+import sys
+
+from stevedore import extension
+import coloredlogs
+
+_pipelines = {}
+
+logger = logging.getLogger('stemworker')
 
 async def authenticate(url, girder_api_key):
     params = {
@@ -10,5 +19,43 @@ async def authenticate(url, girder_api_key):
 
     return resp.cookies['session'].output(header='')
 
+def load_pipelines():
+
+    global _pipelines
+    mgr = extension.ExtensionManager(
+        namespace='stempy.pipeline',
+        invoke_on_load=False,
+    )
+
+    for p in mgr:
+        if p.name in _pipelines:
+            logger.warn('Pipeline already registered with name: %s' % p.name)
+
+        cls = p.entry_point.resolve()
+
+        display_name = None
+        if hasattr(cls, 'NAME'):
+            display_name = cls.NAME
+
+        _pipelines[p.name] = cls
+
+        msg = 'Registered pipeline: %s' % p.name
+        if display_name is not None:
+            msg = '%s - %s' % (msg, display_name)
+        logger.info('Registered pipeline: %s.' % msg)
+
 async def run(url, girder_api_key):
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = coloredlogs.ColoredFormatter('%(asctime)s,%(msecs)03d - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    logger.info('Loading pipelines.')
+    load_pipelines()
+
     cookie = await authenticate(url, girder_api_key)
